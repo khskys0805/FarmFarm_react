667f23791acba648cd91156228ed8f30d509d9f0
khskys0805 <khskys0805@naver.com>
2024-04-05 23:49:46 +0900

    상품 검색 구현

--- src/main/frontend/src/component/TabBar.js
+++ src/main/frontend/src/component/TabBar.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { TiHome } from "react-icons/ti";
 import { SlMenu } from "react-icons/sl";
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
@@ -7,31 +7,45 @@ import { FaShoppingCart } from "react-icons/fa";
 import styles from './TabBar.module.css';
 
 const TabBar = ({ Authorization }) => {
-    const [activeTab, setActiveTab] = useState(null);
     const navigate = useNavigate();
-    const tabLinksRef = useRef([]);
+    const location = useLocation();
 
-    const handleTabClick = (index) => {
-        setActiveTab(index);
+    // 현재 위치에 따라 활성 탭 설정
+    const [activeTab, setActiveTab] = useState(getActiveTab(location.pathname));
+
+    // 경로에 따라 활성 탭을 반환하는 함수
+    function getActiveTab(pathname) {
+        if (pathname === '/home') return 'home';
+        if (pathname === '/menu') return 'menu';
+        if (pathname === '/search') return 'search';
+        if (pathname === '/cart') return 'cart';
+        if (pathname === '/myPage') return 'myPage';
+        return '';
+    }
+
+    // 탭 클릭 시 해당 탭으로 이동 및 활성 탭 설정
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+        navigate(`/${tabName}`);
     };
 
     return (
         <div className={styles.tab}>
             <div className={styles.bar}>
-                <button ref={(el) => tabLinksRef.current[0] = el} className={`tab-link ${activeTab === 0 && 'active'}`} onClick={() => navigate(`/home`)}>
-                    <TiHome style={{ fontSize: '20px' }}/>
+                <button onClick={() => handleTabClick('home')}>
+                    <TiHome style={{ fontSize: '20px', color: activeTab === 'home' ? '#000' : '#d9d9d9' }}/>
                 </button>
-                <button ref={(el) => tabLinksRef.current[1] = el} className={`tab-link ${activeTab === 1 && 'active'}`} onClick={() => handleTabClick(1)}>
-                    <SlMenu style={{ fontSize: '20px' }}/>
+                <button onClick={() => handleTabClick('menu')}>
+                    <SlMenu style={{ fontSize: '20px', color: activeTab === 'menu' ? '#000' : '#d9d9d9' }}/>
                 </button>
-                <button ref={(el) => tabLinksRef.current[2] = el} className={`tab-link ${activeTab === 2 && 'active'}`} onClick={() => navigate(`/search`)}>
-                    <HiMiniMagnifyingGlass style={{ fontSize: '20px' }}/>
+                <button onClick={() => handleTabClick('search')}>
+                    <HiMiniMagnifyingGlass style={{ fontSize: '20px', color: activeTab === 'search' ? '#000' : '#d9d9d9' }}/>
                 </button>
-                <button ref={(el) => tabLinksRef.current[3] = el} className={`tab-link ${activeTab === 3 && 'active'}`} onClick={() => handleTabClick(3)}>
-                    <FaShoppingCart style={{ fontSize: '20px' }}/>
+                <button onClick={() => handleTabClick('cart')}>
+                    <FaShoppingCart style={{ fontSize: '20px', color: activeTab === 'cart' ? '#000' : '#d9d9d9' }}/>
                 </button>
-                <button ref={(el) => tabLinksRef.current[4] = el} className={`tab-link ${activeTab === 4 && 'active'}`} onClick={() => navigate(`/myPage`)}>
-                    <img src="https://farmfarm-bucket.s3.ap-northeast-2.amazonaws.com/7cc20134-7565-44e3-ba1d-ae6edbc213e5.png" className="user-media" alt="" style={{ borderRadius: '50%', width: '25px', height: '25px' }} />
+                <button onClick={() => handleTabClick('myPage')}>
+                    <img src="https://farmfarm-bucket.s3.ap-northeast-2.amazonaws.com/7cc20134-7565-44e3-ba1d-ae6edbc213e5.png" alt="" style={{ borderRadius: '50%', width: '25px', height: '25px'}} />
                 </button>
             </div>
         </div>
--- src/main/frontend/src/pages/search/SearchProduct.js
+++ src/main/frontend/src/pages/search/SearchProduct.js
@@ -1,23 +1,65 @@
 import Header from "../../component/Header";
 import styles from "./SearchProduct.module.css";
 import InputBox from "../../component/InputBox";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {HiMiniMagnifyingGlass} from "react-icons/hi2";
+import TabBar from "../../component/TabBar";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import API from "../../config";
+import ProductList from "../../component/ProductList";
 
 const SearchProduct = () => {
-    const [search, setSearch] = useState(null);
+    const [searchText, setSearchText] = useState("");
+    const [productList, setProductList] = useState([]);
+    const navigate = useNavigate();
 
     const handleInputChange = (e) => {
-        setSearch(e.target.value);
+        setSearchText(e.target.value);
     }
+
+    const filterMonster = productList.filter((item) =>
+        item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    useEffect(() => {
+        axios.get(API.ALLPRODUCT, {
+            headers: { authorization: localStorage.getItem("jwt") },
+        })
+            .then((res) => {
+                console.log("전송 성공");
+                console.log(res.data);
+
+                setProductList(res.data);
+            })
+            .catch((error) => {
+                console.error('작성한 게시물을 가져오는 중 오류 발생: ', error);
+            });
+    }, [])
+    const performSearch = () => {
+        if (searchText === "") {
+            alert('검색어를 입력해주세요.');
+        } else {
+            const url = '/product/list?keyword=' + encodeURIComponent(searchText);
+            navigate(url);
+            console.log("검색어: " + searchText);
+        }
+    };
+
     return (
         <div className={styles.box}>
             <Header title={"상품 검색"} go={`/home`}/>
             <div className={styles.input_wrap}>
-                <InputBox type={"search"} value={search} placeholder={"검색어를 입력해주세요."} onChange={handleInputChange}/>
-                <span><HiMiniMagnifyingGlass style={{ fontSize: '18px' }}/></span>
+                <InputBox type={"search"} value={searchText} placeholder={"검색어를 입력해주세요."} onChange={handleInputChange}/>
+                <span onClick={performSearch}><HiMiniMagnifyingGlass style={{ fontSize: '18px' }}/></span>
             </div>
-
+            {filterMonster && (
+                <div className={styles.search_result}>
+                    <p className={styles.count}>총 <span>{filterMonster.length}</span>개의 상품이 있습니다.</p>
+                    <ProductList products={filterMonster}/>
+                </div>
+            )}
+            <TabBar/>
         </div>
     )
 }
