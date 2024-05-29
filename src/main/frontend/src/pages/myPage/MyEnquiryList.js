c54c90c1c743ce938a1448720bb7524c57f0d1bd
khskys0805 <khskys0805@naver.com>
2024-05-29 17:31:17 +0900

    문의 내역 페이지 완료

--- src/main/frontend/src/pages/myPage/MyEnquiryList.js
+++ src/main/frontend/src/pages/myPage/MyEnquiryList.js
@@ -1,13 +1,16 @@
-import styles from "./MyOrderList.module.css";
+import styles from "./MyEnquiryList.module.css";
 import Header from "../../component/Header";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import API from "../../config";
+import img from "../../images/logo/farmfarm_logo.png";
+import {FaRegStar, FaStar, FaTrashAlt} from "react-icons/fa";
+import {FaPen} from "react-icons/fa6";
 
 const MyEnquiryList = () => {
     const [enquiryList, setEnquiryList] = useState([]);
     useEffect(() => {
-        axios.get(API.MYORDER, {
+        axios.get(API.MYENQUIRY, {
             headers: { authorization: localStorage.getItem("jwt") },
         })
             .then((res) => {
@@ -22,7 +25,34 @@ const MyEnquiryList = () => {
     }, []);
     return (
         <div className={styles.box}>
-            <Header title={"문의 내역"} go={-1}/>
+            <Header title={"문의 내역"} go={`/myPage`}/>
+            <ul>
+                {enquiryList.length === 0 ? (
+                    <div className={styles.no_list}>
+                        <p>작성한 문의가 없습니다.</p>
+                    </div>
+                ) : (
+                    enquiryList.map((enquiry, index) => (
+                        <li key={index} className={styles.enquiry_list}>
+                            <div className={styles.left}>
+                                <div className={styles.img}>
+                                    <img src={img} alt="경매 이미지" />
+                                </div>
+                                <div>
+                                    <h4 className={styles.product_name}>{enquiry.product.name}</h4>
+                                    <p className={styles.content}>{enquiry.content}</p>
+                                </div>
+                            </div>
+                            <div className={styles.right}>
+                                <div className={styles.button}>
+                                    <div><FaPen /></div>
+                                    <div><FaTrashAlt /></div>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                )}
+            </ul>
         </div>
     )
 }
