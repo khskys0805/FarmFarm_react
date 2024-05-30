5b0faa5cf862859c526ea8c58a2980d4d4739574
khskys0805 <khskys0805@naver.com>
2024-05-30 22:10:39 +0900

    지도 마커 띄우기

--- src/main/frontend/src/component/Location.js
+++ src/main/frontend/src/component/Location.js
@@ -11,14 +11,16 @@ const Location = ({ farms }) => {
 
         var map = new kakao.maps.Map(mapContainer, mapOption);
         var geocoder = new kakao.maps.services.Geocoder();
+        console.log(farms);
 
         // farms가 이미 JSON 객체라고 가정
         var farmList = farms;
 
         // farmList를 이용해 필요한 작업 수행
         farmList.forEach(farm => {
-            geocoder.addressSearch(farm.address, function(result, status) {
+            geocoder.addressSearch(farm.locationFull, function(result, status) {
                 if (status === kakao.maps.services.Status.OK) {
+                    console.log('Address search successful:', result);
                     var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
                     var marker = new kakao.maps.Marker({
                         map: map,
@@ -29,6 +31,8 @@ const Location = ({ farms }) => {
                         content: `<div style="padding:5px;">${farm.name}</div>`
                     });
                     infowindow.open(map, marker);
+                } else {
+                    console.error('Geocoder failed: ' + status);
                 }
             });
         });
