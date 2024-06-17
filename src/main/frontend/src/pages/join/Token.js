09e4008bea17a974c2701582154534029be61c0b
khskys0805 <khskys0805@naver.com>
2024-06-18 00:12:12 +0900

    카카오 로그인 비동기 처리

--- src/main/frontend/src/pages/join/Token.js
+++ src/main/frontend/src/pages/join/Token.js
@@ -2,52 +2,54 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import API from "../../config";
-import {BeatLoader} from "react-spinners";
+import { BeatLoader } from "react-spinners";
 
 const Token = () => {
     const navigate = useNavigate();
     const code = new URL(window.location.href).searchParams.get('code');
 
     useEffect(() => {
-        console.log('Token component mounted');
-        if (code) {
-            console.log('Code received:', code); // 디버깅용 콘솔 로그
-            axios.get(API.LOGINTOKEN(code))
-                .then((response) => {
-                    console.log(response.data);
+        const fetchData = async () => {
+            try {
+                console.log('Token component mounted');
+                if (code) {
+                    console.log('Code received:', code); // 디버깅용 콘솔 로그
 
-                    // 토큰을 받아서 localStorage 같은 곳에 저장하는 코드를 여기에 쓴다.
-                    localStorage.setItem('jwt', response.data.result.accessToken); // 일단 이름만 저장했다.
-                })
-                .catch((error) => {
-                    console.error('There was an error!', error);
-                });
-            axios.get(API.ISNICKNAME, {
-                headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` },
-            })
-                .then((response) => {
-                    console.log(response.data);
-                    const nickname = response.data.result.nickname;
+                    // 첫 번째 요청: 토큰 받기
+                    const tokenResponse = await axios.get(API.LOGINTOKEN(code));
+                    console.log(tokenResponse.data);
+
+                    // 토큰을 받아서 localStorage 같은 곳에 저장
+                    localStorage.setItem('jwt', tokenResponse.data.result.accessToken);
+
+                    // 두 번째 요청: 닉네임 확인
+                    const nicknameResponse = await axios.get(API.ISNICKNAME, {
+                        headers: { Authorization: `Bearer ${tokenResponse.data.result.accessToken}` },
+                    });
+                    console.log(nicknameResponse.data);
+
+                    const nickname = nicknameResponse.data.result.nickname;
                     if (nickname) {
                         console.log(nickname);
                         navigate("/home");
-                    }
-                    else{
+                    } else {
                         console.log(nickname);
                         navigate("/nickname");
                     }
-                })
-                .catch((error) => {
-                    console.error('There was an error!', error);
-                });
-        }
+                }
+            } catch (error) {
+                console.error('There was an error!', error);
+            }
+        };
+
+        fetchData();
     }, [code, navigate]);
 
     return (
-        <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BeatLoader color="#36d7b7" />
+        <div style={{ height:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <BeatLoader color="#94C015" />
         </div>
     );
 };
-
 export default Token;
+
