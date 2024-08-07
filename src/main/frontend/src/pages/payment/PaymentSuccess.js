c4f7970414b21934e33494d7c4a457a0aacf9544
khskys0805 <khskys0805@naver.com>
2024-08-08 01:20:35 +0900

    카카오페이 결제 성공시 화면 구성

--- src/main/frontend/src/pages/payment/PaymentSuccess.js
+++ src/main/frontend/src/pages/payment/PaymentSuccess.js
@@ -1,15 +1,23 @@
 import styles from "./Payment.module.css";
 import Button from "../../component/Button";
 import Header from "../../component/Header";
+import { FaCircleCheck } from "react-icons/fa6";
+import {useNavigate} from "react-router-dom";
 
 const PaymentSuccess = () => {
+    const navigate = useNavigate();
     return (
         <div className={styles.box}>
             <Header title={"결제"} />
             <div className={styles.wrapper}>
-                <h1 className={styles.success}>결제에 성공했습니다.</h1>
-                <Button content={"홈 화면으로 가기"} />
-                <Button content={"내 주문 내역 보기"} />
+                <div className={styles.text}>
+                    <h1 className={styles.success}>결제에 성공했습니다.</h1>
+                    <FaCircleCheck size="30px"/>
+                </div>
+                <div className={styles.btn}>
+                    <Button content={"홈 화면으로 가기"} onClick={() => navigate(`/home`)}/>
+                    <Button content={"내 주문 내역 보기"} />
+                </div>
             </div>
         </div>
     )
