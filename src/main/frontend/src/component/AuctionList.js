2cee45e0d84d6654bbb0a1810f48f93b942453ac
khskys0805 <khskys0805@naver.com>
2024-09-19 15:23:51 +0900

    경매 리스트 남은 시간 보여주기

--- src/main/frontend/src/component/AuctionList.js
+++ src/main/frontend/src/component/AuctionList.js
@@ -8,6 +8,41 @@ import {useNavigate} from "react-router-dom";
 const AuctionList = ({ numToShow }) => {
     const { auctionList = [] } = useContext(DataContext); // 기본값을 빈 배열로 설정;
     const navigate = useNavigate();
+    const [timeLeftArray, setTimeLeftArray] = useState([]);
+
+    useEffect(() => {
+        const updateRemainingTime = () => {
+            const updatedTimeLeftArray = auctionList.map(item => calculateTimeLeft(item.closeCalendar));
+            setTimeLeftArray(updatedTimeLeftArray);
+        };
+
+        // 최초 남은 시간 계산
+        updateRemainingTime();
+
+        // 1초마다 시간 갱신
+        const intervalId = setInterval(updateRemainingTime, 1000);
+
+        // 컴포넌트가 언마운트될 때 타이머 정리
+        return () => clearInterval(intervalId);
+    }, [auctionList]);
+
+    const calculateTimeLeft = (closedAt) => {
+        const difference = new Date(closedAt) - new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: String(Math.floor(difference / (1000 * 60 * 60 * 24))).padStart(2, '0'),
+                hours: String(Math.floor((difference / (1000 * 60 * 60)) % 24)).padStart(2, '0'),
+                minutes: String(Math.floor((difference / 1000 / 60) % 60)).padStart(2, '0'),
+                seconds: String(Math.floor((difference / 1000) % 60)).padStart(2, '0'),
+            };
+        } else {
+            timeLeft = { days: '00', hours: '00', minutes: '00', seconds: '00' };
+        }
+
+        return timeLeft;
+    };
 
     const handleGoToAuctionDetail = (item) => {
         navigate(`/auctionDetail/${item.pid}`, { state: { id:item.pid } });
@@ -26,9 +61,18 @@ const AuctionList = ({ numToShow }) => {
                                 <h3 data-date={item.date}></h3>
                             </div>
                             <div className={styles.item_content}>
-                                <h5>{item.farm.name}</h5>
-                                <h3 className={styles.title}>{item.name}</h3>
-                                <h4 className={styles.price}>경매 시작가 {item.price}원</h4>
+                                <div>
+                                    <h5>{item.farm.name}</h5>
+                                    <h3 className={styles.title}>{item.name}</h3>
+                                    <h4 className={styles.price}>경매 시작가 {item.price}원</h4>
+                                </div>
+                                <div>
+                                    {timeLeftArray[index] && (
+                                        <h3>
+                                            {timeLeftArray[index].days}일 {timeLeftArray[index].hours}:{timeLeftArray[index].minutes}:{timeLeftArray[index].seconds}
+                                        </h3>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     )
