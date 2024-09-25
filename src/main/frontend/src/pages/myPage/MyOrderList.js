29cb14788ddf9a202f5aea59ca1916a476356a2a
khskys0805 <khskys0805@naver.com>
2024-09-25 14:28:59 +0900

    주문 내역 수정

--- src/main/frontend/src/pages/myPage/MyOrderList.js
+++ src/main/frontend/src/pages/myPage/MyOrderList.js
@@ -18,8 +18,10 @@ const MyOrderList = () => {
             .then((res) => {
                 console.log("전송 성공");
                 console.log(res.data.result.myOrderList);
-
-                setOrderList(res.data.result.myOrderList);
+                const myOrder = res.data.result.myOrderList.filter((item) =>
+                    item.orderDetails[0].type != 2
+                );
+                setOrderList(myOrder);
             })
             .catch((error) => {
                 console.error('작성한 게시물을 가져오는 중 오류 발생: ', error);
@@ -31,48 +33,54 @@ const MyOrderList = () => {
     }
     return (
         <div className={styles.box}>
-            <Header title={"주문 내역"} go={`/myPage`}/>
+            <Header title={"주문 내역"} go={`/myPage`} />
             <ul>
                 {orderList.length === 0 ? (
                     <div className={styles.no_list}>
-                        <p>아직 주문 내역이 없습니다!<br/>
+                        <p>아직 주문 내역이 없습니다!<br />
                             상품을 구매해보세요!!</p>
                         <Button content={"판매 상품 보러가기"} onClick={handleOrderProduct} />
                     </div>
                 ) : (
                     orderList.map((order, index) => (
-                        <>
+                        <div key={index}>
                             <div className={styles.order_date}>
-                                {order.status === "결제 완료" && (<div className={styles.order_cancel}>주문취소</div>)}
-                                <h4 className={styles.date}>{new Date(order.created_at).toLocaleDateString('ko-KR', {
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })}</h4>
-                                <p className={styles.order_status}>{order.status}</p>
+                                {order.paymentStatus === "PAYMENT_COMPLETED" && (
+                                    <div className={styles.order_cancel}>주문취소</div>
+                                )}
+                                <h4 className={styles.date}>
+                                    {new Date(order.created_at).toLocaleDateString('ko-KR', {
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric'
+                                    })}
+                                </h4>
+                                <p className={styles.order_status}>{order.paymentStatus}</p>
                             </div>
-                            <li key={index} className={styles.order_list}>
-                                <div className={styles.left}>
-                                    <div className={styles.img}>
-                                        <img src={order.orderDetails[0].fileUrl} alt="상품 이미지" />
+                            {order.orderDetails.map((detail, detailIndex) => (
+                                <li key={detailIndex} className={styles.order_list}>
+                                    <div className={styles.left}>
+                                        <div className={styles.img}>
+                                            <img src={detail.images[0].fileUrl} alt="상품 이미지" />
+                                        </div>
+                                        <div>
+                                            <p className={styles.farm_name}>{detail.farmName}</p>
+                                            <h5 className={styles.product_name}>{detail.productName}</h5>
+                                            <h4 className={styles.price}>{detail.price}원</h4>
+                                            <h4 className={styles.total_price}>총 금액: {detail.totalPrice}원</h4>
+                                        </div>
                                     </div>
                                     <div>
-                                        <p className={styles.farm_name}>{order.orderDetails[0].farmName}</p>
-                                        <h5 className={styles.product_name}>{order.orderDetails[0].productName}</h5>
-                                        <h4 className={styles.price}>{order.orderDetails[0].price}원</h4>
-                                        <h4 className={styles.total_price}>총 금액: {order.orderDetails[0].totalPrice}원</h4>
+                                        <h4 className={styles.review} onClick={() => navigate(`/review/write`)}>리뷰 작성</h4>
+                                        <h4 className={styles.quantity}>X {detail.quantity}</h4>
                                     </div>
-                                </div>
-                                <div>
-                                    <h4 className={styles.review} onClick={() => navigate(`/review/write`)}>리뷰 작성</h4>
-                                    <h4 className={styles.quantity}>X {order.orderDetails[0].quantity}</h4>
-                                </div>
-                            </li>
-                        </>
+                                </li>
+                            ))}
+                        </div>
                     ))
                 )}
             </ul>
         </div>
-    )
+    );
 }
 export default MyOrderList;
