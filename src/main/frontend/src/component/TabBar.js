eddd0d3701d6700431a2c718dd485c674e55092f
khskys0805 <khskys0805@naver.com>
2024-11-07 15:06:45 +0900

    탭바 프로필 이미지 반영

--- src/main/frontend/src/component/TabBar.js
+++ src/main/frontend/src/component/TabBar.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { TiHome } from "react-icons/ti";
 import { SlMenu } from "react-icons/sl";
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 import { FaShoppingCart } from "react-icons/fa";
 import styles from './TabBar.module.css';
+import axios from "axios";
+import API from "../config";
 
 const TabBar = ({ Authorization }) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const [profileImage, setProfileImage] = useState("");
+
+    useEffect(() => {
+        axios.get(API.MYPAGE, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('jwt')}` },
+        })
+            .then((res) => {
+                setProfileImage(res.data.result.profileImage);
+            })
+            .catch((error) => {
+                console.error('작성한 게시물을 가져오는 중 오류 발생: ', error);
+            });
+    }, [])
 
     // 현재 위치에 따라 활성 탭 설정
     const [activeTab, setActiveTab] = useState(getActiveTab(location.pathname));
@@ -45,7 +60,11 @@ const TabBar = ({ Authorization }) => {
                     <FaShoppingCart style={{ fontSize: '20px', color: activeTab === 'cart' ? '#000' : '#d9d9d9' }}/>
                 </button>
                 <button onClick={() => handleTabClick('myPage')}>
-                    <img src="https://farmfarm-bucket.s3.ap-northeast-2.amazonaws.com/7cc20134-7565-44e3-ba1d-ae6edbc213e5.png" alt="" style={{ borderRadius: '50%', width: '25px', height: '25px'}} />
+                    {profileImage ? (
+                        <img src={profileImage} style={{ borderRadius: '50%', width: '25px', height: '25px'}} />
+                    ) : (
+                        <img src="https://farmfarm-bucket.s3.ap-northeast-2.amazonaws.com/7cc20134-7565-44e3-ba1d-ae6edbc213e5.png" alt="" style={{ borderRadius: '50%', width: '25px', height: '25px'}} />
+                    )}
                 </button>
             </div>
         </div>
