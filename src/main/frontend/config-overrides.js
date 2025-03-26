4f740ab1a3b81eb497d297711cd71c42d5f286b3
khskys0805 <khskys0805@naver.com>
2025-03-26 22:02:55 +0900

    fix: config-overrides.js 수정

--- src/main/frontend/config-overrides.js
+++ src/main/frontend/config-overrides.js
@@ -1,11 +1,14 @@
-// config-overrides.js
 module.exports = function override(config, env) {
     if (env === 'development') {
         config.devServer = {
             ...config.devServer,
             client: {
-                webSocketURL: 'wss://farm-farm.store/ws',
-            },
+                webSocketURL: {
+                    hostname: 'farm-farm.store',
+                    port: '443', // HTTPS라면 443 포트 사용
+                    protocol: 'wss' // 🔥 웹소켓을 보안 연결(wss)로 강제 설정
+                }
+            }
         };
     }
     return config;
